ec73308dfa629e9175001e355ef2a517bfcb27a3
ikopmniko <hannapitman786@gmail.com>
2025-12-07 22:43:04 +0700

    Update worker_gemini.py

--- worker_gemini.py
+++ worker_gemini.py
@@ -12,12 +12,12 @@ from google import genai  # SDK baru: google-genai
 # ============================
 JOBS_API_URL = "https://leamarie-yoga.de/jobs_api.php"  # GANTI ke URL jobs_api.php kamu
 
-MIN_SECONDS_PER_REQUEST = 10
-MAX_RETRIES_PER_TITLE = 3
+MIN_SECONDS_PER_REQUEST = 61
+MAX_RETRIES_PER_TITLE = 1
 DEFAULT_QUOTA_SLEEP_SECONDS = 60
 
 # Batas maksimal request ke Gemini per API key / worker
-MAX_REQUESTS_PER_API = 25  # <<< BATAS REQUEST PER API KEY
+MAX_REQUESTS_PER_API = 230  # <<< BATAS REQUEST PER API KEY
 
 
 # ============================
@@ -324,6 +324,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
